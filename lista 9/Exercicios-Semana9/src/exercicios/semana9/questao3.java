8fab0c9e94ad6ba6f0d61cec456c854aaf909ed5
eemilyy <emilyalmeeidas@gmail.com>
2022-04-25 00:53:30 -0300

    add the question 3

---
+++ lista 9/Exercicios-Semana9/src/exercicios/semana9/questao3.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicios.semana9;
+
+/**
+ *
+ * @author emily
+ */
+public class questao3 {
+    
+    static void corrigirImagem(int[][] img){
+        int max = 0;
+        int min = 255;        
+        for(int i = 0; i < img.length; i++){
+            for(int j = 0; j < img[0].length; j++){
+                max = Math.max(max, img[i][j]);
+                min = Math.min(min, img[i][j]);
+            }
+        }
+        
+        System.out.println(max);
+        System.out.println(min);
+        
+        // Correção de escala
+        for(int i = 0; i < img.length; i++){
+            for(int j = 0; j < img[0].length; j++){
+                img[i][j] = (255 * (img[i][j] -  min))/(max-min);
+            }
+        }
+    }
+        
+    public static void main(String[] args) {
+        int[][] img = ImagemDigital.carregarImagem("./imagens/Fig0229(a)(tungsten_filament_shaded).png");
+        int[][] img2 = ImagemDigital.carregarImagem("./imagens/Fig0229(b)(tungsten_sensor_shading).png");
+        int[][] newImg = new int[img.length][img[0].length];
+        
+        ImagemDigital.plotarImagem(img, "Imagem 1");
+        ImagemDigital.plotarImagem(img2, "Imagem 2");
+        
+        for(int i = 0; i < img.length; i++){
+            for(int j = 0; j < img[0].length; j++){
+                newImg[i][j] = (img[i][j] / img2[i][j]);
+            }
+        }
+        
+        corrigirImagem(img);
+        ImagemDigital.plotarImagem(img, "Imagem com Divisão");
+    }    
+}
