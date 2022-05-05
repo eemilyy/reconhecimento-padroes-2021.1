892a0866d02378322f9058d8709d770189e230d6
eemilyy <emilyalmeeidas@gmail.com>
2022-05-05 01:07:04 -0300

    finished the question 3

--- lista 10/exercicio-lista10/src/exercicio/lista10/questao3.java
+++ lista 10/exercicio-lista10/src/exercicio/lista10/questao3.java
@@ -37,20 +37,22 @@ public class questao3 {
     
     public static void main(String[] args) {
         
-        // FALTA FAZEEEEEEEEEEEEEERRRRRRRRRRRR
         int[][] img = ImagemDigital.carregarImagem("./imagens/Fig0308(a)(fractured_spine).png");
         int[][] newImg = new int[img.length][img[0].length];
         int[][] newImg2 = new int[img.length][img[0].length];
         int[][] newImg3 = new int[img.length][img[0].length];
+        
+        double gama1 = 0.6;
+        double gama2 = 0.5;
+        double gama3 = 0.4;
 
         ImagemDigital.plotarImagem(img, "Imagem 1");
         
         for(int i = 0; i < img.length; i++){
             for(int j = 0; j < img[0].length; j++){
-                newImg[i][j] = (int) ((double)((255 - 1) / log(0.6, 255) * Math.log(1 + img[i][j]))); // c*log(1+r)
-                newImg2[i][j] = (int) ((255 - 1) / log(255, 0.5) * Math.log(1 + img[i][j]));
-                newImg3[i][j] = (int) ((255 - 1) / log(255, 0.4) * Math.log(1 + img[i][j]));
-                //newImg3[i][j] = (int) ((255 - 1 / log(img[i][j], 0.4) * Math.log(1 + img[i][j])));
+                newImg[i][j] = (int) Math.pow(img[i][j], gama1);
+                newImg2[i][j] = (int) Math.pow(img[i][j], gama2);
+                newImg3[i][j] = (int) Math.pow(img[i][j], gama3);
             }
         }
         
@@ -58,8 +60,8 @@ public class questao3 {
         corrigirImagem(newImg2);
         corrigirImagem(newImg3);
         ImagemDigital.plotarImagem(newImg, "Imagem Gama 0.6");
-        //ImagemDigital.plotarImagem(newImg2, "Imagem Gama 0.5");
-        //ImagemDigital.plotarImagem(newImg3, "Imagem Gama 0.4");
+        ImagemDigital.plotarImagem(newImg2, "Imagem Gama 0.5");
+        ImagemDigital.plotarImagem(newImg3, "Imagem Gama 0.4");
     }
     
 }
